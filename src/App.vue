295d00a322f5df9e12cb9f778155230058fab922
kevin <kehyoungnim@gmail.com>
2021-03-04 01:35:53 +0900

    map lib code move

--- src/App.vue
+++ src/App.vue
@@ -10,30 +10,7 @@ export default {
   name: "App",
   components: {
     HelloWorld,
-  }, //,
-  // mounted() {
-  //   if (window.kakao && window.kakao.maps) {
-  //     this.initMap();
-  //   } else {
-  //     const script = document.createElement("script");
-  //     /* global kakao */
-  //     script.onload = () => kakao.maps.load(this.initMap);
-  //     script.src =
-  //       "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=97ef3485b87cdb41adf506a8c42c308d";
-  //     document.head.appendChild(script);
-  //   }
-  // },
-  // methods: {
-  //   initMap() {
-  //     var mapContainer = document.getElementById("map"), // 지도를 표시할 div
-  //       mapOption = {
-  //         center: new kakao.maps.LatLng(37.564343, 126.947613), // 지도의 중심좌표
-  //         level: 3, // 지도의 확대 레벨
-  //       };
-
-  //     var map = new kakao.maps.Map(mapContainer, mapOption);
-  //   },
-  // },
+  },
 };
 </script>
 
